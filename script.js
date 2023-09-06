d8bd1bc279d58eeb4e433359c9ef25deaa84ca41
Victor Manuel <vmag2407@hotmail.com>
2023-09-06 14:29:51 -0500

    add js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const headerBk = document.querySelector('.container__header--payments')
+const headerBk = document.querySelector('.container__header--transparent')
+const navHeader = document.querySelectorAll('.nav__servicios-header')
+console.log( navHeader)
 
 window.addEventListener('scroll', () => {
     if (scrollY === 0) {
 
         headerBk.style.background = 'transparent';
-    } else {
+        headerBk.style.transition = ' all .25s';
+        navHeader.forEach(nav => {
+            nav.style.color = 'white';
+        });
+        
+    }
+    else {
+        navHeader.forEach(nav => {
+            nav.style.color = '';
+        });
         headerBk.style.background = '#FFF';
 
     }
