48324411482858404081da64ff9f3cf5c656920b
Victor Manuel <vmag2407@hotmail.com>
2023-09-06 14:11:56 -0500

    add header blanco

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-.then(res => res.json())
-.then(data => {
-    console.log(data)
+const headerBk = document.querySelector('.container__header--payments')
+
+window.addEventListener('scroll', () => {
+    if (scrollY === 0) {
+
+        headerBk.style.background = 'transparent';
+    } else {
+        headerBk.style.background = '#FFF';
+
+    }
 })
